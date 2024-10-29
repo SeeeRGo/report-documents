466d4fefff8a017fdb1474c9cc721213da2bd82b
Мизин Сергей Юрьевич (4091265) <mizin@vtb.ru>
2024-10-29 18:55:16 +0400

    ui fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,70 @@
-import Image from "next/image";
+'use client'
 
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import OpenAI from "openai";
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
+import { useState } from "react";
+
+export const client = new OpenAI({
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true
+});
+const createGetResponse = (propmpt: ChatCompletionMessageParam[]) => async (): Promise<any> => {
+  if (!propmpt.length) return [{}]
+  const response = await client.chat.completions.create({
+    model: 'gpt-4o-mini',
+    messages: propmpt,
+    response_format: {"type": "json_object"}
+  })
+  .asResponse()
+  const json = await response.json()
+  const content = json.choices.at(0)?.message.content
+  // const parsed = content ? JSON.parse(content) : {}
+  return content
+}
+const createPrompt = ({
+  promptBody,
+  schemaJson,
+  userInput,
+}: {promptBody: string, schemaJson: string, userInput: string}): ChatCompletionMessageParam[] => {
+  return   [
+    {"role": "system", "content": `Extract information from message according this JSON schema - ${schemaJson}.
+    ${promptBody}
+  `},
+  {"role": "user", "content": userInput }
+  ]
+}
 export default function Home() {
+  const [input, setInput] = useState('')
+  const [schema, setSchema] = useState('')
+  const [prompt, setPrompt] = useState('')
+  const [output, setOutput] = useState('')
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="grid items-start p-8 pb-20 gap-4 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="grid w-full gap-1.5">
+        <Label>Input</Label>
+        <Textarea value={input} onChange={(e) => setInput(e.target.value)} id="input" />      
+      </div>
+      <div className="grid w-full gap-1.5">
+        <Label>JSON Schema</Label>
+        <Textarea value={schema} onChange={(e) => setSchema(e.target.value)} id="json" />      
+      </div>
+      <div className="grid w-full gap-1.5">
+        <Label>Prompt</Label>
+        <Textarea value={prompt} onChange={(e) => setPrompt(e.target.value)} id="prompt" /> 
+      </div>
+      <Button onClick={async () => {
+        const constructedPrompt = createPrompt({ promptBody: prompt, schemaJson: schema, userInput: input})
+        const getResponse = createGetResponse(constructedPrompt)
+        const response = await getResponse()
+        setOutput(response)
+      }}>Generate Output</Button>  
+      <label>Output</label>
+      <p>
+        {output || 'No output yet'}
+      </p>
     </div>
   );
 }
